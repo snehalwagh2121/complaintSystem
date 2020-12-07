fecfb2f985806797c17997bdf4d7ead4121fcf66
Snehal <snehalwagh2121@gmail.com>
2020-12-07 18:16:51 +0530

    sessions and post complaint

--- controller/users.js
+++ controller/users.js
@@ -1,7 +1,7 @@
 const dbObj=require('../server.js');
 const userSchema=require('../models/userSchema.js');
 const userOperations={
-    getUsers(user){
+    authenticate(user){
        return new Promise((resolve, reject)=>{
            const authSql="select * from users where username='"+user.userId+"' and password ='"+user.password+"'";
            dbObj.db.query(authSql, (err, result)=>{
@@ -9,7 +9,7 @@ const userOperations={
                 if(err)
                     return reject( new Error({msg: err}));
                 else if(result.length>0)
-                    return resolve({msg: 'User created successfully'});
+                    return resolve({user: result[0]});
                 else
                     return reject( new Error({msg: 'Invalid Username or Password'}))
                 });
@@ -23,7 +23,19 @@ const userOperations={
                 if(err)
                     return reject( new Error({msg: err}));
                 else
-                    return resolve({msg: 'User created successfully'});
+                    return resolve({user: result[0]});
+            });
+        })
+    },
+    addComplaint(complaint,imageName,userId){
+        return new Promise((resolve, reject)=>{
+            const addComplaint= `insert into complaint(uid, complaint, image, district) values(${userId},'${complaint.grievanceDtl}','${imageName}','${complaint.district}')`;
+            dbObj.db.query(addComplaint, (err, result)=>{
+                console.log('query executed : '+addComplaint);
+                if(err)
+                    return reject( new Error({msg: err}));
+                else
+                    return resolve({msg: 'complaint regitered successfully'});
             });
         })
     }
--- routes/UsersRequests.js
+++ routes/UsersRequests.js
@@ -2,29 +2,38 @@ const users=require('../controller/users.js')
 const express=require('express');
 const bodyParser= require('body-parser');
 const multer = require('multer');
+const session=require('express-session');
 
 var urlEncodedParser= bodyParser.urlencoded({extended : false})
 const router=express.Router();
 
+router.use(session({
+    secret: 'secret-key',
+    resave: false,
+    saveUninitialized: false
+}));
 const storage = multer.diskStorage({
     destination: (req, file, callBack) => {
         callBack(null, 'public/img');
     },
     filename: (req, file, callBack) => {
-        callBack(null, `RIMG_${file.originalname}`)
+        callBack(null, `${file.originalname}`)
     }
   });
 var upload = multer({ storage: storage })
 
 
-router.post('/login',(req, res)=>{
-    console.log('get user login call made from UserRequire.js');
+router.post('/login',urlEncodedParser,(req, res)=>{
+    console.log('post user login call made from UserRequire.js');
+    console.log(req.body);
     users.authenticate(req.body)
     .then((result)=>{
-        console.log('success', result);
-        res.render('postComplaint');
+        console.log('success', result.user);
+        req.session.user=result.user;
+        res.redirect('postComplaint');
     }).catch((error)=>{
-        console.log('error: '+error);
+        console.log('error: '+error.error);
+        req.session.user=null;
         res.redirect('/home');
     });
 });
@@ -32,6 +41,7 @@ router.post('/signUp',urlEncodedParser, (req, res)=>{
     users.createUser(req.body)
     .then((result)=>{
         console.log('success', result);
+        req.session.user=result.user;
         res.redirect('/postComplaint');
     }).catch((error)=>{
         console.log('error: '+error);
@@ -41,15 +51,21 @@ router.post('/signUp',urlEncodedParser, (req, res)=>{
 
 router.get('/postComplaint', (req, res)=>{
     console.log('showing post complaint view');
-    res.render('users/postComplaint',{userData: {userId: 'snehalwagh2121@gmail.com', district: 'ahmednagar'}});
+    if(req.session.user!=null){
+        res.render('users/postComplaint',{userData: req.session.user});
+    }else{
+        res.redirect('/home');
+    }
 });
 router.post('/postComplaint',upload.single('image'), urlEncodedParser, (req, res)=>{
-    if(req.file){
-        console.log(req.file);
-    }
-    console.log(req.body);
-
-    // const addComplaint= `insert into complaint(uid, complaint, image, district) values(,'${req.body.grievanceDtl}','${req.file.filename}','${req.body.district}')`;
-    res.render('users/postComplaint',{userData: {userId: 'snehalwagh2121@gmail.com', district: 'ahmednagar'}});
+    console.log('comaplint: ',req.body);
+    users.addComplaint(req.body,req.file.filename, req.session.user.uid)
+    .then((result)=>{
+        console.log(result);
+        res.redirect('/home');
+    }).catch((error)=>{
+        console.log('error: '+error);
+        res.redirect('/home');
+    });
 });
 module.exports=router;
